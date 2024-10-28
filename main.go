6d57826f18806c7cf97b43a2f0d9fb78d7107274
iftekhar <iftekhar@divine-it.net>
2024-10-28 16:45:41 +0600

    <feature><insert, get users>
    insert users, get users, compare time

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gocql/gocql"
@@ -17,10 +19,10 @@ type UserRole struct {
 // User struct to hold user data
 type User struct {
 	ID       string    `json:"id"`
-	Username string    `json:"username"` // Changed from Name to Username
+	Username string    `json:"username"`
 	Email    string    `json:"email"`
-	Password string    `json:"password"` // New field for password
-	Value1   string    `json:"value1"`   // Example for additional values
+	Password string    `json:"password"`
+	Value1   string    `json:"value1"`
 	Value2   string    `json:"value2"`
 	Value3   string    `json:"value3"`
 	Value4   string    `json:"value4"`
@@ -202,6 +204,16 @@ func main() {
 	})
 
 	router.GET("/users", func(ctx *gin.Context) {
+
+		limitStr := ctx.DefaultQuery("limit", "10")
+		limit, err := strconv.Atoi(limitStr)
+		if err != nil || limit <= 0 {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid limit parameter"})
+			return
+		}
+
+		startTime := time.Now()
+
 		var (
 			id       string
 			username string
@@ -342,9 +354,140 @@ func main() {
 			return
 		}
 
+		duration := time.Since(startTime)
+
 		// Return users in JSON format
-		ctx.JSON(http.StatusOK, users)
+		ctx.JSON(http.StatusOK, gin.H{
+			"time_taken": duration.Seconds(),
+			"users":      users,
+		})
 	})
+
+	router.GET("/insert", func(ctx *gin.Context) {
+
+		insertCount := ctx.DefaultQuery("count", "1")
+		count, err := strconv.Atoi(insertCount)
+		if err != nil || count <= 0 {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid count parameter"})
+			return
+		}
+
+		startTime := time.Now()
+
+		var users []User
+
+		for i := 0; i < count; i++ {
+			user := User{
+				ID:       fmt.Sprintf("user_%d", i+1),
+				Username: fmt.Sprintf("User%d", i+1),
+				Email:    fmt.Sprintf("user%d@example.com", i+1),
+				Password: "securepassword",
+				Value1:   fmt.Sprintf("Value1_%d", i+1),
+				Value2:   fmt.Sprintf("Value2_%d", i+1),
+				Value3:   fmt.Sprintf("Value3_%d", i+1),
+				Value4:   fmt.Sprintf("Value4_%d", i+1),
+				Value5:   fmt.Sprintf("Value5_%d", i+1),
+				Value6:   fmt.Sprintf("Value6_%d", i+1),
+				Value7:   fmt.Sprintf("Value7_%d", i+1),
+				Value8:   fmt.Sprintf("Value8_%d", i+1),
+				Value9:   fmt.Sprintf("Value9_%d", i+1),
+				Value10:  fmt.Sprintf("Value10_%d", i+1),
+				Value11:  fmt.Sprintf("Value11_%d", i+1),
+				Value12:  fmt.Sprintf("Value12_%d", i+1),
+				Value13:  fmt.Sprintf("Value13_%d", i+1),
+				Value14:  fmt.Sprintf("Value14_%d", i+1),
+				Value15:  fmt.Sprintf("Value15_%d", i+1),
+				Value16:  fmt.Sprintf("Value16_%d", i+1),
+				Value17:  fmt.Sprintf("Value17_%d", i+1),
+				Value18:  fmt.Sprintf("Value18_%d", i+1),
+				Value19:  fmt.Sprintf("Value19_%d", i+1),
+				Value20:  fmt.Sprintf("Value20_%d", i+1),
+				Value21:  fmt.Sprintf("Value21_%d", i+1),
+				Value22:  fmt.Sprintf("Value22_%d", i+1),
+				Value23:  fmt.Sprintf("Value23_%d", i+1),
+				Value24:  fmt.Sprintf("Value24_%d", i+1),
+				Value25:  fmt.Sprintf("Value25_%d", i+1),
+				Value26:  fmt.Sprintf("Value26_%d", i+1),
+				Value27:  fmt.Sprintf("Value27_%d", i+1),
+				Value28:  fmt.Sprintf("Value28_%d", i+1),
+				Value29:  fmt.Sprintf("Value29_%d", i+1),
+				Value30:  fmt.Sprintf("Value30_%d", i+1),
+				Value31:  fmt.Sprintf("Value31_%d", i+1),
+				Value32:  fmt.Sprintf("Value32_%d", i+1),
+				Value33:  fmt.Sprintf("Value33_%d", i+1),
+				Value34:  fmt.Sprintf("Value34_%d", i+1),
+				Value35:  fmt.Sprintf("Value35_%d", i+1),
+				Value36:  fmt.Sprintf("Value36_%d", i+1),
+				Value37:  fmt.Sprintf("Value37_%d", i+1),
+				Value38:  fmt.Sprintf("Value38_%d", i+1),
+				Value39:  fmt.Sprintf("Value39_%d", i+1),
+				Value40:  fmt.Sprintf("Value40_%d", i+1),
+			}
+
+			if err := session.Query(
+    "INSERT INTO my_saas.users (id, email, password, username, value1, value10, value11, value12, value13, value14, value15, value16, value17, value18, value19, value2, value20, value21, value22, value23, value24, value25, value26, value27, value28, value29, value3, value30, value31, value32, value33, value34, value35, value36, value37, value38, value39, value4, value40, value5, value6, value7, value8, value9) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    user.ID,
+    user.Email,
+    user.Password,
+    user.Username,
+    user.Value1,
+    user.Value10,
+    user.Value11,
+    user.Value12,
+    user.Value13,
+    user.Value14,
+    user.Value15,
+    user.Value16,
+    user.Value17,
+    user.Value18,
+    user.Value19,
+    user.Value2,
+    user.Value20,
+    user.Value21,
+    user.Value22,
+    user.Value23,
+    user.Value24,
+    user.Value25,
+    user.Value26,
+    user.Value27,
+    user.Value28,
+    user.Value29,
+    user.Value3,
+    user.Value30,
+    user.Value31,
+    user.Value32,
+    user.Value33,
+    user.Value34,
+    user.Value35,
+    user.Value36,
+    user.Value37,
+    user.Value38,
+    user.Value39,
+    user.Value4,
+    user.Value40,
+    user.Value5,
+    user.Value6,
+    user.Value7,
+    user.Value8,
+    user.Value9,
+).Exec(); err != nil {
+    ctx.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Error inserting user: %v", err)})
+    return
+}
+
+			users = append(users, user)
+
+		}
+
+		duration := time.Since(startTime)
+
+		// Return users in JSON format
+		ctx.JSON(http.StatusOK, gin.H{
+			"message":    fmt.Sprintf("%d users inserted successfully", count),
+			"time_taken": duration.Seconds(),
+		})
+	})
+
 	// Start the server
 	router.Run(":8080")
 }
